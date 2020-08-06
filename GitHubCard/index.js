731669e704b4d41324d0e72f63132b45e9913631
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-08-06 14:44:02 -0500

    made the axios requests and cards and stuff

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,12 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/buchananta')
+  .then(response => console.log(response.data))
+  .catch(error => console.error("CAUGHT ERROR" + error));
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -16,7 +19,10 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
-
+const cards = document.querySelector('.cards')
+axios.get('https://api.github.com/users/buchananta')
+  .then(res => cards.appendChild(makeCard(res.data)))
+  .catch(error => console.error('CAUGHT ERROR:' + error))
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -28,12 +34,28 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+    'torvalds',
+    'tetondan',
+    'dustinmyers',
+    'justsml',
+    'luishrd',
+    'bigknell',
+];
+followersArray.forEach(person => {
+  axios.get(`https://api.github.com/users/${person}`)
+    .then(res => {
+      cards.appendChild(makeCard(res.data))
+    })
+    .catch(error => {
+      return console.error(`error retrieving ${person}` + error)
+    })
+  }
+)
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
-
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
@@ -49,7 +71,24 @@ const followersArray = [];
       </div>
     </div>
 */
-
+function makeCard(data) {
+  const element = document.createElement('div');
+  element.className = 'card';
+  element.innerHTML = `<img src = ${data.avatar_url} />
+  <div class="card-info">
+    <h3 class="name">${data.name}</h3>
+    <p class="username">${data.login}</p>  
+    <p>Location: ${data.location}</p>
+    <p>Profile:
+      <a href=${data.html_url}>${data.html_url}</a>
+      </p>
+      <p>Followers: ${data.followers}</p>
+      <p>Following: ${data.following}</p>
+      <p>Bio: ${data.bio}</p>
+      </div>
+    </div>`
+  return element;
+}
 /*
   List of LS Instructors Github username's:
     tetondan
